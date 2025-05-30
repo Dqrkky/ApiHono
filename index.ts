7f9995f7e98fad6890a8344475a0eca7af0cc532
Dqrk <70263848+Dqrkky@users.noreply.github.com>
2025-05-30 15:55:17 +0300

    update

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-import { inject } from "@vercel/analytics"
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
+import { inject } from "@vercel/analytics";
 import { injectSpeedInsights } from '@vercel/speed-insights';
-
+import 'dotenv/config';
+ 
+inject({
+});
 injectSpeedInsights();
 
 const app = new Hono()
@@ -14,6 +17,36 @@ app.get('/', (c) => {
 
 api.get('/', (c) => {
   return c.json({ message: 'Hello Hono!' })
+})
+
+api.post('/webhooks/:webhookName', async (c) => {
+  const webhookName = c.req.param('webhookName')
+  const body = await c.req.json()
+  const data = {
+    webhook: {
+      name: webhookName,
+      data: body
+    }
+  }
+  const forwardUrl = process.env.FORWARD_URL as string
+  if (!forwardUrl) {
+    return c.json({ status: 'error', message: 'Missing environment variable: FORWARD_URL' }, 500)
+  }
+  try {
+    const response = await fetch(forwardUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    })
+    return c.json({
+      status: 'ok',
+    })
+  } catch (error) {
+    return c.json({
+      status: 'error',
+      message: (error as Error).message 
+    }, 500)
+  }
 })
 
 export const config = {
